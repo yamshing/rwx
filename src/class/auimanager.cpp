2177ff1a9a821cbf5a8d2fc84577472051634195
Shingo Yamamoto <yamshing@gmail.com>
2021-12-17 17:14:59 +0900

    tool bar image add

--- src/class/auimanager.cpp
+++ src/class/auimanager.cpp
@@ -18,9 +18,7 @@ AuiManager::AuiManager(int nargs, VALUE *args)
 	wxToolBar* m_tb=new wxToolBar(parent_p,wxID_ANY,wxDefaultPosition,wxDefaultSize,wxTB_FLAT|wxTB_NODIVIDER);
 	m_tb->AddTool(wxID_ANY,wxT("tool1"),wxArtProvider::GetBitmap(wxART_FILE_OPEN,wxART_TOOLBAR));
 	m_tb->AddTool(wxID_ANY,wxT("tool2"),wxArtProvider::GetBitmap(wxART_FILE_SAVE,wxART_TOOLBAR));
-	//m_tb->AddTool(wxID_ANY,wxT("tool1"),wxBitmap(16,15));
-	//m_tb->AddTool(wxID_ANY,wxT("tool2"),wxBitmap(16,15));
-
+	 
 	m_tb->Realize();
 	 
 	wxAuiPaneInfo pi = wxAuiPaneInfo()  .Name(wxT("toolbar"))
