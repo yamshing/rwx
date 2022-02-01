b00cd3d401fc87a3cb21dc9bcfa128d3804f4fbc
Shingo Yamamoto <yamshing@gmail.com>
2022-02-01 14:44:35 +0900

    simple mod sample add

---
+++ sample/simple_mod.rb
@@ -0,0 +1,25 @@
+
+module Rwx
+	 
+	class Frame
+		def on_init 
+			@panel = Panel.new(self)
+		end
+	end
+	 
+	class App
+		def on_init
+			 
+			@simple = Simple.new
+			p @simple
+			 
+			@frame = Frame.new
+			@frame.set_size(1000, 500)
+			 
+		end
+	end
+end
+	 
+app = Rwx::App.new
+
+
