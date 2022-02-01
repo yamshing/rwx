a7ebb1597e125ad8987e0efe813fd12a56d8e12e
Shingo Yamamoto <yamshing@gmail.com>
2022-02-01 11:39:26 +0900

    include module name ready

--- include_module.sh
+++ include_module.sh
@@ -6,7 +6,14 @@ if  test $1 = "linux" ; then
 	 
 	echo "linux";
 	 
-	#sed -e "s/\/\*DEFINE\*\//\/\*DEFINE\*\/\nhoge/g" src/ruby/librwx/librwx_base.c > src/ruby/librwx/librwx.c
+	define_str=""
+	for name in ./mod/*
+	do
+		echo $(basename ${name})
+		define_str="${define_str} $(basename ${name})\n"
+	done
+	 
+	sed -e "s/\/\*DEFINE\*\//\/\*DEFINE\*\/\n${define_str}/g" src/ruby/librwx/librwx_base.c > src/ruby/librwx/librwx.c
 	 
 else
 	 
