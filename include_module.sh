3b982d02a918e5e0eb7f2c98c7ba504b0be0d297
Shingo Yamamoto <yamshing@gmail.com>
2022-02-01 14:37:29 +0900

    rwx define call add

--- include_module.sh
+++ include_module.sh
@@ -7,13 +7,18 @@ if  test $1 = "linux" ; then
 	echo "linux";
 	 
 	define_str=""
+	call_str=""
 	for name in ./mod/*
 	do
 		echo $(basename ${name})
-		define_str="${define_str} $(basename ${name})\n"
+		filename=$(basename ${name})
+		define_str="${define_str}`cat ${name}/rwx_define`"
+		call_str="${call_str}`cat ${name}/rwx_call`"
+		 
 	done
 	 
 	sed -e "s/\/\*DEFINE\*\//\/\*DEFINE\*\/\n${define_str}/g" src/ruby/librwx/librwx_base.c > src/ruby/librwx/librwx.c
+	sed -i -e "s/\/\*CALL\*\//\/\*CALL\*\/\n${call_str}/g" src/ruby/librwx/librwx.c
 	 
 else
 	 
