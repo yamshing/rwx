4cdb4c1795cef84ffea379d60050bfc98a170db6
Shingo Yamamoto <yamshing@gmail.com>
2022-03-01 10:28:34 +0900

    multi frame sample add

---
+++ sample/multi_frame.rb
@@ -0,0 +1,45 @@
+module Rwx
+	class Panel
+		def on_init 
+			 
+			out_sizer = Sizer.new('box', 'vertical')
+			row_sizer = Sizer.new('box', 'horizontal')
+			button_1 = Button.new(self, label:'Click Here',cb_inst:self, cb_name:'on_button_click' )
+			row_sizer.add_spacer(20)
+			row_sizer.add(button_1)
+			 
+			out_sizer.add_spacer(20)
+			out_sizer.add(row_sizer)
+			add_sizer(out_sizer)
+			 
+		end
+		 
+		def on_button_click
+			@sub_frame = SubFrame.new
+			@sub_frame.set_size(500,500);
+		end
+		 
+	end
+	 
+	class SubFrame < Frame
+		def on_init 
+		end
+	end
+	 
+	 
+	class Frame
+		def on_init 
+			@panel = Panel.new(self)
+		end
+	end
+	 
+	class App
+		def on_init
+			@frame = Frame.new
+			@frame.set_size(1000, 500)
+		end
+	end
+end
+ 
+app = Rwx::App.new
+ 
