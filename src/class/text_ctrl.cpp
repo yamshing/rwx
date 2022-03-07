098456f92e5cf91587f723dc6cea5981b354c38f
Shingo Yamamoto <yamshing@gmail.com>
2022-03-07 17:15:47 +0900

    text ctrl size option add

--- src/class/text_ctrl.cpp
+++ src/class/text_ctrl.cpp
@@ -10,11 +10,48 @@ TextCtrl::TextCtrl(int nargs, VALUE *args)
 	App* app_p = static_cast<App*>(wxTheApp);
 	VALUE parent = args[0];
 	VALUE content = args[1];
+	 
+	VALUE option = 0;
+	 
+	if (nargs > 2) {
+		option = args[2];
+	}
+	 
 	std::string content_str; 
 	StaticFunc::ValueToString(content, content_str);
 	wxWindow* parent_p = static_cast<wxWindow*>(app_p->GetObjectFromMap(parent));
-	m_text_ctrl_p = new wxTextCtrl(parent_p, wxID_ANY, wxString::FromUTF8(content_str) );
 
+	 
+	if (option != 0) {
+		 
+		if (rb_funcall(option, rb_intern("has_key?"),1,ID2SYM(rb_intern("style")))) {
+
+			VALUE style = rb_hash_aref(option, ID2SYM(rb_intern("style")));
+			std::string style_str = std::string(StringValuePtr(style));
+			int w = 150;
+			int h = 200;
+
+			if (rb_funcall(option, rb_intern("has_key?"),1,ID2SYM(rb_intern("w")))) {
+				VALUE w_val = rb_hash_aref(option, ID2SYM(rb_intern("w")));
+				w = NUM2INT(w_val);
+			}
+			if (rb_funcall(option, rb_intern("has_key?"),1,ID2SYM(rb_intern("h")))) {
+				VALUE h_val = rb_hash_aref(option, ID2SYM(rb_intern("h")));
+				h = NUM2INT(h_val);
+			}
+			 
+			if (style_str == "multi_line") {
+				m_text_ctrl_p = new wxTextCtrl(parent_p, wxID_ANY, wxString::FromUTF8(content_str), wxDefaultPosition,wxSize(w, h), wxTE_MULTILINE);
+			}
+			 
+		}
+		 
+	}else{
+
+		m_text_ctrl_p = new wxTextCtrl(parent_p, wxID_ANY, wxString::FromUTF8(content_str) );
+		 
+	}
+	 
 }
 
 
