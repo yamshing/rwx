4ded7a781c9d3824a043ee3e01f56c2637a8320d
Shingo Yamamoto <yamshing@gmail.com>
2022-06-03 13:57:48 +0900

    sizer demo add

--- sample/sizer.rb
+++ sample/sizer.rb
@@ -13,7 +13,7 @@ module Rwx
 			 
 			@sizer = Sizer.new('box', 'vertical')
 			row_1_sizer = Sizer.new('box', 'horizontal')
-			@text_ctrl = TextCtrl.new(@frame, 'hello',{'style':'multi_line','w':500, 'h':400})
+			@text_ctrl = TextCtrl.new(@frame, 'hello',{'style':'multi_line','w':9999, 'h':9999})
 			row_1_sizer.add(@text_ctrl)
 			 
 			@sizer.add(row_1_sizer)
